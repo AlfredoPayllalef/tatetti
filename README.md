5128b0c59979ba9ec194772defbc17b94c635bdf
lucianaromano <luciana.romano@est.fi.uncoma.edu.ar>
2021-11-28 19:18:58 -0300

    actualizacion

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Universidad del Comahue
 
 ## integrantes del equipo
 Alfredo Nicolas Payllalef FAI 3496 Mail: alfredo.payllalef@est.fi.uncoma.edu.ar Usuario de github AlfredoPayllalef
+Fabian Facundo Muñoz Huinolpan FAI 1787 Mail: Fabianchock@Gmail.com Usuario de github  threeIslands3
+Luciana Romano  FAI 3075 Mail: luciana.romano@est.fi.uncoma.edu.ar Usuario de github lucianaromano
---
+++ programaRomanoPayllalefMuñoz.php
@@ -0,0 +1,69 @@
+<?php
+include_once("tateti.php");
+
+/**************************************/
+/***** DATOS DE LOS INTEGRANTES *******/
+/**************************************/
+
+/* Apellido, Nombre. Legajo. Carrera. mail. Usuario Github */
+/*Alfredo Nicolas Payllalef FAI 3496 Mail: alfredo.payllalef@est.fi.uncoma.edu.ar Usuario de github AlfredoPayllalef
+Fabian Facundo Muñoz Huinolpan FAI 1787 Mail: Fabianchock@Gmail.com Usuario de github  threeIslands3
+Luciana Romano  FAI 3075 Mail: luciana.romano@est.fi.uncoma.edu.ar Usuario de github lucianaromano 
+*/
+
+
+
+
+
+/**************************************/
+/***** DEFINICION DE FUNCIONES ********/
+/**************************************/
+
+
+
+
+
+
+
+
+/**************************************/
+/*********** PROGRAMA PRINCIPAL *******/
+/**************************************/
+
+//Declaración de variables:
+
+
+//Inicialización de variables:
+
+
+//Proceso:
+
+$juego = jugar();
+//print_r($juego);
+//imprimirResultado($juego);
+
+
+
+/*
+do {
+    $opcion = ...;
+
+    
+    switch ($opcion) {
+        case 1: 
+            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 1
+
+            break;
+        case 2: 
+            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 2
+
+            break;
+        case 3: 
+            //completar qué secuencia de pasos ejecutar si el usuario elige la opción 3
+
+            break;
+        
+            //...
+    }
+} while ($opcion != X);
+*/
