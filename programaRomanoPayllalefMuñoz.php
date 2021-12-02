0e45a92eabd40fcee23f0dd8e2c10ec327251d67
Fabian <fabianchock@gmail.com>
2021-12-02 12:36:56 -0300

    Add funcion resumen jugador
    Esta funcion analiza una coleccion de juegos y el nombre de un jugador
    para devolver un resumen de las estadisticas del jugador.

--- programaRomanoPayllalefMuñoz.php
+++ programaRomanoPayllalefMuñoz.php
@@ -155,33 +155,31 @@ function agregarjuego($agregarjuego, $nombre, $simbolo, $puntos){
 /**  recibe por parametro una coleccion de juegos y el nombre de un jugador para retornar el resumen de la estructura b    
  * @param array $coleccionDeJuegos
  * @param string $nombreJugador
- * @var int $puntosJug0 , $puntosJugx, $i
- * @var array $resumen
- * @return array
-*/
-function resumenJugador($coleccionDeJuegos,$nombreJugador){
- 
+ * @var int $puntosJug0 , $puntosJugx, $i 
+ * @return STRING
+ */
+function resumenJugador($coleccionDeJuegos, $nombreJugador)
+{
+
     $nombreJugador = strtoupper($nombreJugador);
     $tamañoArreglo = count($coleccionDeJuegos);
     $i = 0;
     $puntosJug0 = 0;
     $puntosJugx = 0;
-    $resumen = [];
-    while($i < $tamañoArreglo){
-       //evaluamos si el jugador participo en X o en O
-       if($coleccionDeJuegos[$i]["jugadorCruz"] == $nombreJugador){
-       $puntosJugx = $puntosJugx + $coleccionDeJuegos[$i]["puntosCruz"];
-       }
-       elseif($coleccionDeJuegos[$i]["jugadorCirculo"] == $nombreJugador){
-       $puntosJug0 = $puntosJug0 + $coleccionDeJuegos[$i]["puntosCirculo"];
+    while ($i < $tamañoArreglo) {
+        //evaluamos si el jugador participo en X o en O
+        if ($coleccionDeJuegos[$i]["jugadorCruz"] == $nombreJugador) {
+            $puntosJugx = $puntosJugx + $coleccionDeJuegos[$i]["puntosCruz"];
+        } elseif ($coleccionDeJuegos[$i]["jugadorCirculo"] == $nombreJugador) {
+            $puntosJug0 = $puntosJug0 + $coleccionDeJuegos[$i]["puntosCirculo"];
+        }
+        $i = $i + 1;
     }
-    $i = $i +1;
-    }
-    $resumen[0]=$nombreJugador;
-    $resumen[1]=$puntosJugx;
-    $resumen[2]=$puntosJug0;
-    $resumen[3]=$puntosJug0+$puntosJugx;
-    return $resumen;
+    $resumenDeJugador = ("nombre jugador: " . $nombreJugador . "\n
+puntos como jugador cruz: " . $puntosJugx . "\n
+puntos como jugador circulo: " . $puntosJug0 . "\n
+puntos en total: " . $puntosJug0 + $puntosJugx . "");
+    return $resumenDeJugador;
 }
 
 /**
